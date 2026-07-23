bce308c8cd8e48d94052b8739d75baea3812224e
Catriel Pereira <catopereira72@gamil.com>
2026-07-23 15:57:45 -0400

    refactor(routing): update app routes and configuration
    Switched eager loaded components to use loadComponent for better performance, added RouterOutlet, and fixed tsconfig.spec.json.

--- BattleTanks-Frontend/src/app/app.routes.ts
+++ BattleTanks-Frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { 
+    path: '', 
+    loadComponent: () => import('./components/waiting-room/waiting-room').then(m => m.WaitingRoom) 
+  },
+  { 
+    path: 'game', 
+    loadComponent: () => import('./components/game-canvas/game-canvas').then(m => m.GameCanvas) 
+  },
+];
